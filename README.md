e88bdccf4780baa1cfc44c25aa82000b1b2ddad6
Dan Trusca <truscadani@yahoo.com>
2021-03-08 14:52:21 +0200

    initial setup for socket.IO with connect and disconnect

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# chat-app-socketIO
+<div>
+    <h3>Socket connection</h3>
+    <ul>
+        <li>https://socket.io/get-started/chat/</li>
+    </ul>
+</div>
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const app = require("express")();
-const http = require("http").createServer(app);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  //   res.send("<h1>Hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("a user has just disconnected");
+  });
 });
 
 http.listen(3000, () => {
